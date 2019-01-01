4641b1606125754603a3fc4444cc9a60326f7545
Peter Christie <peterchristie@Peters-MacBook-Pro.local>
2018-12-31 17:26:57 -0700

    first commit, react hook testing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, useState, useEffect, Suspense, lazy } from 'react';
 import './App.css';
-
+const SuspenseTest = lazy(() => import("./lib/SuspenseTest"));
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Testhook />
       </div>
     );
   }
 }
+
+function Testhook() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState('Peter');
+  const [showPDFPreview, setShowPDFPreview] = useState(false);
+
+  useEffect(() => {
+    document.title = name;
+  })
+
+  function handleClick(){
+    console.log("peter");
+    setCount(count + 1)
+    if(!showPDFPreview){
+      setShowPDFPreview(true);
+    }
+  }
+
+  return(
+    <div>
+      <p>You clicked {count} times!</p>
+      <button onClick={()=>handleClick()}>Try Clicking me!</button>
+      <input value={name} onChange={(e)=>setName(e.target.value)} />
+      {showPDFPreview && (
+      <Suspense maxDuration={500} fallback={<div>Loading...</div>}>
+        <SuspenseTest />
+      </Suspense>
+      )}
+    </div>
+  );  
+};
 
 export default App;
